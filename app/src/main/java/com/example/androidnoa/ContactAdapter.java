d5a37670bedef464aa1c14f29d402fcd0a1838ea
OrHaroni <or.haroni@gmail.com>
2023-06-11 15:58:49 +0300

    Added Contacts view

---
+++ app/src/main/java/com/example/androidnoa/ContactAdapter.java
@@ -0,0 +1,84 @@
+package com.example.androidnoa;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
+
+public class ContactAdapter extends BaseAdapter {
+
+    List<Contact> contactList;
+
+    private class ViewHolder {
+        int id;
+        TextView displayName;
+        TextView lastMessageDate;
+        TextView lastMessageContent;
+        ImageView profilePic;
+    }
+
+    //Constructor
+    public ContactAdapter(List<Contact> contactList) {
+        this.contactList = contactList;
+    }
+
+    @Override
+    public int  getCount(){
+        return this.contactList.size();
+    }
+
+    @Override
+    public Contact getItem(int id){
+        for (Contact contact : this.contactList){
+            if(contact.id == id){
+                return contact;
+            }
+        }
+        return null;
+    }
+
+    //So the adapter wont be abstract
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        if (convertView == null) {
+            convertView = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.contact_layout, parent, false);
+
+            ViewHolder viewHolder = new ViewHolder();
+            viewHolder.displayName = convertView.findViewById(R.id.display_name);
+            viewHolder.id = 1; //Fictive id
+            viewHolder.profilePic = convertView.findViewById(R.id.Profile_Pic);
+            viewHolder.lastMessageContent = convertView.findViewById(R.id.last_message_content);
+            viewHolder.lastMessageDate = convertView.findViewById(R.id.last_message_date);
+
+            convertView.setTag(viewHolder);
+        }
+
+        Contact c = this.contactList.get(position);
+        ViewHolder viewHolder = (ViewHolder) convertView.getTag();
+        viewHolder.displayName.setText(c.getDisplayName());
+        viewHolder.profilePic.setImageResource(c.getProfilePic());
+        viewHolder.lastMessageContent.setText(c.getLastMessageContent());
+        viewHolder.lastMessageDate.setText(c.getLastMessageDate());
+
+        viewHolder.profilePic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(v.getContext(), "Clicked on image", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        return convertView;
+    }
+}
