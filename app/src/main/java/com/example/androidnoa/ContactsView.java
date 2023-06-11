103069a16b20e420748da655e7ca72af327f94f6
OrHaroni <or.haroni@gmail.com>
2023-06-11 15:58:49 +0300

    Added Contacts view

---
+++ app/src/main/java/com/example/androidnoa/ContactsView.java
@@ -0,0 +1,44 @@
+package com.example.androidnoa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ListView;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class ContactsView extends AppCompatActivity {
+
+    List<Contact> contactList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contacts_view);
+
+        ListView lstFeed = (ListView) findViewById(R.id.lstContacts);
+
+        contactList = generateContacts();
+        final ContactAdapter feedAdapter = new ContactAdapter(contactList);
+        lstFeed.setAdapter(feedAdapter);
+
+    }
+
+    private List<Contact> generateContacts() {
+        List<Contact> contacts = new ArrayList<>();
+
+        int default_pic = getResources().getIdentifier("default_pic", "drawable", getPackageName());
+        contacts.add(new Contact(1,
+                "Or Haroni", "Last Message", "Date", default_pic));
+        contacts.add(new Contact(2,
+                "Naor Nahman", "Last Message", "Date"));
+        contacts.add(new Contact(3,
+                "Adar Katz", "Last Message", "Date", default_pic));
+
+        return contacts;
+    }
+
+}
--- app/src/main/java/com/example/androidnoa/MainActivity.java
+++ app/src/main/java/com/example/androidnoa/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.androidnoa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Openning the CONTACTS LIST
+        Intent intent = new Intent(MainActivity.this, ContactsView.class);
+        startActivity(intent);
     }
 }
