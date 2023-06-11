4f710dd30842b2ab443083d46469898c5879c57d
Naor <naornahmman@gmail.com>
2023-06-11 15:25:56 +0300

    register page

---
+++ app/src/main/java/com/example/androidnoa/RegisterActivity.java
@@ -0,0 +1,67 @@
+package com.example.androidnoa;
+
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.widget.Button;
+import android.widget.ImageView;
+import androidx.activity.result.ActivityResultLauncher;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+public class RegisterActivity extends AppCompatActivity {
+    int PICK_IMAGE = 1;
+    ImageView imageViewPicture;
+    Button buttonAddPicture;
+    ActivityResultLauncher<Intent> galleryLauncher;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_activity);
+
+        imageViewPicture = findViewById(R.id.imageViewPicture);
+        buttonAddPicture = findViewById(R.id.buttonAddPicture);
+        Button btnRegister = findViewById(R.id.btnRegister);
+        Button btnAlreadyHaveAnAccount = findViewById(R.id.btnAlreadyHaveAnAccount);
+        galleryLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result.getResultCode() == RESULT_OK) {
+                        Intent data = result.getData();
+                        if (data != null) {
+                            Uri selectedImage = data.getData();
+                            try {
+                                InputStream inputStream = getContentResolver().openInputStream(selectedImage);
+                                Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                                imageViewPicture.setImageBitmap(bitmap);
+                            } catch (FileNotFoundException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                });
+        btnRegister.setOnClickListener(v -> {
+
+            //Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+        //    startActivity(intent);
+        });
+        btnAlreadyHaveAnAccount.setOnClickListener(v -> {
+           // Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+         //   startActivity(intent);
+        });
+    }
+
+
+    private void openGallery() {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        galleryLauncher.launch(intent);
+    }
+}
+
