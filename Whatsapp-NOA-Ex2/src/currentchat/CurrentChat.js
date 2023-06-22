4e3030d10cf7db478c40bcc091a50cddc86ec4f8
adarkatz <adkat80@gmail.com>
2023-06-22 12:12:37 +0300

    did the project with the server

--- Whatsapp-NOA-Ex2/src/currentchat/CurrentChat.js
+++
@@ -1,9 +0,0 @@
-function CurrentChat(props) {
-    return (
-        <div id="current-chat-info">
-            <img src={props.groupPic} className="rounded-circle profile-pic-in-div" />
-             <span id="chat-name">{props.groupName}</span>
-        </div>
-    );
-}
-export default CurrentChat; 
--- Whatsapp-NOA-Ex2/src/database/Database.js
+++
@@ -1,8 +0,0 @@
- export const userList = ([]);
- 
-
-  // Returning a user (variable) by the id
-  export const getUserById = (id) => {
-    return userList.find((user) => user.id === id);
-  };
-
